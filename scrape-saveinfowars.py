caceccddcef42d63de476c5c43d094036f4686a6
Egarok <egarokrocks@gmail.com>
2021-11-24 07:09:30 -0800

    Updated scraper file to update donations records csv if new rows to be added

--- scrape-saveinfowars.py
+++ scrape-saveinfowars.py
@@ -5,16 +5,27 @@ import cloudscraper
 # URL to latest donation data
 url = "https://www.givesendgo.com/donation/recentdonations?camp=44611&donation=null"
 
+# returns a CloudScraper instance
 scraper = cloudscraper.create_scraper()
 
 res = scraper.get(url=url)
 data = res.json()
-df = pd.DataFrame(data['returnData']['donations'])
+donations_data = data['returnData']['donations']
+df_recent = pd.DataFrame(donations_data)
 
-# df_previous = pd.read_csv('data/saveinfowars-funding.csv', index_col=False)
-# print(df_previous)
+csv_filename = 'data/saveinfowars-donations-records.csv'
 
+df_compiled = pd.read_csv(csv_filename, index_col=False)
+
+for index, row in df_recent.iterrows():
+    if row['donation_id'] not in df_compiled['donation_id'].tolist():
+        # source: add row to top of DataFrame (optimize later?)
+        # https://stackoverflow.com/questions/43408621/add-a-row-at-top-in-pandas-dataframe
+
+        df_compiled.loc[-1] = row # add new row
+        df_compiled.index = df_compiled + 1 # shift index
+        df_compiled.sort_index(inplace=True)
 
 # write to data file
-df.to_csv('data/saveinfowars-funding.csv', sep=',', index=False)
+df_compiled.to_csv(csv_filename, sep=',', index=False)
 
